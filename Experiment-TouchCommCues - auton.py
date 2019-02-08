10d225b9148b7e92341f56f26e4a467bbf156f0f
Sarah McIntyre <sarah.mcintyre@liu.se>
2019-02-08 11:47:51 +0100

    initial commit

---
+++ Experiment-TouchCommCues - auton.py
@@ -0,0 +1,204 @@
+from psychopy import visual, core, event, data, gui, parallel
+import numpy as np
+import random, os, pygame, copy
+from touchcomm import *
+
+
+# -- GET INPUT FROM THE EXPERIMENTER --
+
+exptInfo = {'00. Experiment name':'touch-comm-auton',
+            '01. Participant Code':'test',
+            '02. Number of repeats':5,
+            '03. Inter-stimulus interval (sec)':50,
+            '04. Require response on (n) bonus trials':5,
+            '05. Participant screen':0,#1,
+            '06. Participant screen resolution':'800,600', #'1920, 1200',
+            '07. Experimenter screen':0,
+            '08. Experimenter screen resolution':'400,300', #'1280,720',
+            '09. Play audio cue for video sync':True,
+            '10. Send signal for biopac sync':('none','serial','parallel'),
+            '11. Port address':'COM5', #'0x3FF8',
+            '12. Folder for saving data':'data'}
+
+
+dlg = gui.DlgFromDict(exptInfo, title='Experiment details')
+if dlg.OK:
+    pass ## continue
+else:
+    core.quit() ## the user hit cancel so exit
+
+exptInfo['14. Date and time']= data.getDateStr(format='%Y-%m-%d_%H-%M-%S') ##add the current time
+
+
+# text displayed to experimenter and participant
+displayText = {'startMessage': 'Press Space to start.',
+                'waitMessage': 'Please wait.',
+                'continueMessage': 'Press space for the audio cue.',
+                'touchMessage': 'Follow the audio cue.',
+                'fixationMessage': '+',
+                'finishedMessage': 'The session has finished.',
+                'finishedSyncMessage': 'The session has finished.\n Unplug the headphones, then press space to play the video sync sound.'}
+if exptInfo['09. Play audio cue for video sync']:
+    displayText['startMessage'] = 'Make sure speakers are plugged in, not headphones. Then press space to start. Plug in headphones after the video sync sound has played.'
+
+# ----
+
+
+# -- SETUP STIMULUS RANDOMISATION AND CONTROL --
+
+stimLabels = ['attention','gratitude','love','sadness','happiness','calming']
+receiverCueText = dict((line.strip().split('\t') for line in open('receiver-cues.txt')))
+toucherCueText = dict((line.strip().split('\t') for line in open('toucher-cues.txt')))
+soundDurations = dict((line.strip().split('\t') for line in open('./sounds/durations.txt')))
+
+stimList = []
+for stim in stimLabels: 
+    stimList.append({'stim':stim,
+                    'toucherCueText':toucherCueText[stim],
+                    'receiverCueText':receiverCueText[stim],
+                    'cueSound':'./sounds/{} - short.wav' .format(stim),
+                    'cueSoundDuration':float(soundDurations[stim]),
+                    'SignalNo':stimLabels.index(stim)+1})
+trials = data.TrialHandler(stimList, exptInfo['02. Number of repeats'])
+
+# ----
+
+# -- MAKE FOLDER/FILES TO SAVE DATA --
+
+saveFiles = DataFileCollection(foldername = exptInfo['12. Folder for saving data'],
+                filename = exptInfo['00. Experiment name'] + '_' + exptInfo['14. Date and time'] +'_P' + exptInfo['01. Participant Code'],
+                headers = ['trial','cued','response','correct'],
+                dlgInput = exptInfo)
+
+# ----
+
+# -- SETUP VISUAL INTERFACE --
+
+toucher = DisplayInterface(False,
+                        exptInfo['07. Experimenter screen'],
+                        [int(i) for i in exptInfo['08. Experimenter screen resolution'].split(',')], ## convert text input to numbers
+                        displayText['startMessage'])
+
+receiver = ButtonInterface(False,
+                            exptInfo['05. Participant screen'],
+                            [int(i) for i in exptInfo['06. Participant screen resolution'].split(',')],
+                            displayText['waitMessage'],
+                            2,3,
+                            [receiverCueText[i] for i in stimLabels])
+
+# -----
+
+# -- SETUP AUDIO --
+
+pygame.mixer.pre_init() 
+pygame.mixer.init()
+goStopSound = pygame.mixer.Sound('./sounds/go-stop.wav')
+
+# ----
+
+
+# -- SETUP DATA SYNC --
+if exptInfo['09. Play audio cue for video sync']:
+    audioSync = './sounds/sync.wav'
+else: audioSync = None
+
+sync = DataSync(audioSync,
+                portType = exptInfo['10. Send signal for biopac sync'],
+                portAddress = exptInfo['11. Port address'],
+                portResetCode = 0,
+                portBonusStimCode = len(stimLabels)+1,
+                portEndStimCode = len(stimLabels)+2,
+                portSyncCode = len(stimLabels)+3)
+
+# ----
+
+
+# -- RUN THE EXPERIMENT --
+
+# display starting screens
+exptClock=core.Clock()
+exptClock.reset()
+isiCountdown = core.CountdownTimer(0)
+receiver.startScreen(displayText['waitMessage'])
+toucher.startScreen(displayText['startMessage'])
+
+# wait for start trigger
+for (key,keyTime) in event.waitKeys(keyList=['space','escape'], timeStamped=exptClock):
+    if key in ['escape']:
+        saveFiles.logAbort(keyTime)
+        core.quit()
+    if key in ['space']:
+        exptClock.add(keyTime)
+        saveFiles.logEvent(0,'experiment started')
+
+# signal the start of the experiment
+sync.sendSyncPulse()
+
+totalTrials = trials.nTotal + exptInfo['04. Require response on (n) bonus trials']
+nTrialsComplete = 0
+
+# start the main experiment loop
+for thisTrial in trials:
+    
+    event.clearEvents()
+    
+    
+    # bonus trial
+    if trials.thisN % (trials.nTotal/exptInfo['04. Require response on (n) bonus trials']) == 0:
+        
+        randomStimTrial = copy.deepcopy(random.sample(stimList,1)[0])
+        randomStimTrial['SignalNo'] = sync.bonusStim
+        
+        if nTrialsComplete == 0: isiCountdown.reset(min(5,exptInfo['03. Inter-stimulus interval (sec)']))
+        present_stimulus(randomStimTrial,exptInfo,displayText,receiver,toucher,saveFiles,exptClock,isiCountdown,goStopSound,sync)
+        
+        response = get_response(stimLabels,receiverCueText,randomStimTrial,displayText,receiver,toucher,saveFiles,exptClock)
+        
+        nTrialsComplete +=1
+        
+        saveFiles.writeTrialData([nTrialsComplete,
+                        randomStimTrial['stim'],
+                        response,
+                        int(randomStimTrial['stim']==response)])
+        
+        saveFiles.logEvent(exptClock.getTime(),'{} of {} complete' .format(nTrialsComplete, totalTrials))
+    
+    # regular trial
+    if nTrialsComplete == 0: isiCountdown.reset(10)
+    present_stimulus(thisTrial,exptInfo,displayText,receiver,toucher,saveFiles,exptClock,isiCountdown,goStopSound,sync)
+    
+    nTrialsComplete +=1
+    response = 'none'
+    
+    saveFiles.writeTrialData([nTrialsComplete,
+                            thisTrial['stim'],
+                            response,
+                            int(thisTrial['stim']==response)])
+    
+    saveFiles.logEvent(exptClock.getTime(),'{} of {} complete' .format(nTrialsComplete, totalTrials))
+
+# -----
+
+# prompt at the end of the experiment
+event.clearEvents()
+receiver.updateMessage(displayText['finishedMessage'])
+toucher.updateMessage(displayText['finishedMessage'])
+
+if exptInfo['09. Play audio cue for video sync']:
+    toucher.updateMessage(displayText['finishedSyncMessage'])
+    # wait for finish trigger
+    for (key,keyTime) in event.waitKeys(keyList=['space','escape'], timeStamped=exptClock):
+        if key in ['escape']:
+            saveFiles.logAbort(keyTime)
+            core.quit()
+        if key in ['space']:
+            pass
+    # signal the end of the experiment
+    sync.sendSyncPulse()
+
+saveFiles.logEvent(exptClock.getTime(),'experiment finished')
+saveFiles.closeFiles()
+core.wait(2)
+receiver.win.close()
+toucher.win.close()
+core.quit()
---
+++ touchcomm.py
@@ -0,0 +1,395 @@
+from psychopy import visual, event, core
+import numpy as np
+import random, os, pygame, time, math, serial
+
+class DataFileCollection():
+    def __init__(self,foldername,filename,headers,dlgInput):
+        self.folder = './'+foldername+'/'
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+        self.fileprefix = self.folder + filename
+        
+        self.infoFile = open(self.fileprefix+'_info.csv', 'w') 
+        for k,v in dlgInput.items(): self.infoFile.write(k + ',' + str(v) + '\n')
+        self.infoFile.close()
+        
+        self.dataFile = open(self.fileprefix+'_data.csv', 'w')
+        self.writeTrialData(headers)
+        
+        self.logFile = open(self.fileprefix+'_log.csv', 'w')
+        self.logFile.write('time,event\n')
+    
+    def logEvent(self,time,event):
+        self.logFile.write('{},{}\n' .format(time,event))
+        print('LOG: {} {}' .format(time, event))
+    
+    def closeFiles(self):
+        self.dataFile.close()
+        self.logFile.close()
+    
+    def logAbort(self,time):
+        self.logEvent(time,'experiment aborted')
+        self.closeFiles()
+    
+    def writeTrialData(self,trialData):
+        lineFormatting = ','.join(['{}']*len(trialData))+'\n'
+        self.dataFile.write(lineFormatting.format(*trialData))
+
+class DisplayInterface:
+    def __init__(self,fullscr,screen,size,message):
+        self.textColour = [-1,-1,-1]
+        
+        self.win = visual.Window(fullscr = fullscr, 
+                                    allowGUI = True, 
+                                    screen = screen,
+                                    size = size)
+        
+        self.message = visual.TextStim(self.win,
+                                        text = message,
+                                        height = 0.12,
+                                        color = self.textColour,
+                                        units = 'norm',
+                                        pos = (0,-0))
+        
+        self.timerDisplay = visual.TextStim(self.win,
+                                        text = '',
+                                        height = 0.12,
+                                        color = self.textColour,
+                                        units = 'norm',
+                                        pos = (0.8,-0.8))
+    def updateMessage(self,message):
+        self.message.text = message
+        self.win.flip()
+    
+    def startScreen(self,message):
+        self.message.text = message
+        self.message.autoDraw = True
+        event.clearEvents()
+        self.win.flip()
+    
+    def updateTimerDisplay(self,timer):
+        self.timerDisplay.text = str(int(math.ceil(timer)))
+        self.timerDisplay.autoDraw = True
+        self.win.flip()
+    
+    def hideTimerDisplay(self):
+        self.timerDisplay.text = ''
+        self.timerDisplay.autoDraw = False
+        self.win.flip()
+
+class ButtonInterface(DisplayInterface):
+    def __init__(self,fullscr,screen,size,message,nCol,nRow,buttonLabels):
+        DisplayInterface.__init__(self,fullscr,screen,size,message)
+        self.nButtons = nCol*nRow
+        self.outlineColour = [-1,-1,-1]
+        self.buttonWidth = 0.6
+        self.buttonHeight = 0.2
+        self.buttonColour = [0,.25,.9]
+        self.mouse = event.Mouse(True,None,self.win)
+        
+        ##evenly space the buttons from each other and edges
+        xpos = np.linspace(-1,1,nCol+2)[1:nCol+1]
+        ypos = -np.linspace(-1,1,nRow+2)[1:nRow+1]
+        self.buttonPosition = []
+        for x in xpos:
+            for y in ypos:
+                self.buttonPosition += [(x,y)]
+        
+        self.buttons = []
+        self.buttonText = []
+        for n in range(self.nButtons):
+            self.buttons += [visual.Rect(self.win,
+                                    width = self.buttonWidth,
+                                    height= self.buttonHeight,
+                                    fillColor = self.buttonColour,
+                                    lineColor = self.outlineColour,
+                                    units = 'norm',
+                                    pos = self.buttonPosition[n])]
+            self.buttonText += [visual.TextStim(self.win,
+                                    text=buttonLabels[n],
+                                    height=self.buttonHeight/3,
+                                    wrapWidth = self.buttonWidth,
+                                    color = self.textColour,
+                                    units = 'norm',
+                                    pos = self.buttonPosition[n])]
+    
+    def showButtons(self,buttonLabels):
+        self.mouse.clickReset()
+        for n in range(self.nButtons):
+            self.buttonText[n].text = buttonLabels[n]
+            self.buttons[n].opacity = 1
+            self.buttons[n].autoDraw = True
+            self.buttonText[n].autoDraw = True
+        self.win.flip()
+    
+    def hideButtons(self):
+        for n in range(self.nButtons):
+            self.buttonText[n].autoDraw = False
+            self.buttons[n].autoDraw = False
+        self.win.flip()
+    
+    def getButtonClick(self,clock):
+        event.clearEvents()
+        self.mouse.clickReset()
+        mouseResetTime = clock.getTime()
+        clicked = False
+        aborted = False
+        while not clicked and not aborted:
+            for n, button in enumerate(self.buttons):
+                if self.mouse.isPressedIn(button, buttons=[0]):
+                    mbutton, tList = self.mouse.getPressed(getTime=True)
+                    t = tList[0] + mouseResetTime
+                    clicked = True
+                    response = n
+                    break
+                ## is the mouse inside the shape (hovering over it)?
+                if button.contains(self.mouse):
+                    button.opacity = 0.3
+                else:
+                    button.opacity = 1
+                self.win.flip()
+                time.sleep(0.001)
+            for (key,t) in event.getKeys(['escape'], timeStamped=clock):
+                response = -2
+                aborted = True
+        return (response,t)
+    
+    def getSelection(self,timeout,clock):
+        event.clearEvents()
+        confirmed = False
+        aborted = False
+        fwd = ['a','down']
+        bwd = ['b','up']
+        conf = ['c','return']
+        quit = ['escape']
+        buttonSelected = -1
+        response = -1
+        countDown = core.CountdownTimer()
+        countDown.add(timeout)
+        while not aborted and not confirmed and countDown.getTime() > 0:
+            for (key,t) in event.getKeys(fwd+bwd+conf+quit, timeStamped=clock):
+                if key in fwd:
+                    self.buttons[buttonSelected].opacity = 1
+                    buttonSelected = (buttonSelected+1) % self.nButtons
+                    self.buttons[buttonSelected].opacity = 0.3
+                if key in bwd:
+                    self.buttons[buttonSelected].opacity = 1
+                    if buttonSelected < 0:
+                        buttonSelected = buttonSelected % self.nButtons
+                    else:
+                        buttonSelected = (buttonSelected-1) % self.nButtons
+                    self.buttons[buttonSelected].opacity = 0.3
+                if key in conf and buttonSelected >= 0:
+                    self.buttons[buttonSelected].opacity = 1
+                    response = buttonSelected
+                    confirmed = True
+                if key in quit:
+                    self.buttons[buttonSelected].opacity = 1
+                    response = -2
+                    aborted = True
+                self.win.flip()
+        if countDown.getTime() <= 0: t = clock.getTime()
+        self.buttons[buttonSelected].opacity = 1
+        self.win.flip()
+        return (response,t)
+
+class DataSync():
+    def __init__(self,audioSync = None, portType = None, portAddress = None, portResetCode = 0,portBonusStimCode =1, portEndStimCode =9, portSyncCode = 10):
+        
+        if audioSync!=None:
+            self.audioOn = True
+            pygame.mixer.pre_init() 
+            pygame.mixer.init()
+            self.syncSound = pygame.mixer.Sound(audioSync)
+        else:
+            self.audioOn = False
+        
+        self.portType = portType
+        self.reset = portResetCode
+        self.bonusStim = portBonusStimCode
+        self.endStim = portEndStimCode
+        self.syncPulse = portSyncCode
+        
+        if self.portType == 'parallel':
+            self.port = parallel.ParallelPort(portAddress)
+            self.port.setData(self.reset)
+        elif self.portType == 'serial':
+            self.port = serial.Serial(portAddress,9600,timeout = 0.05)
+            self.port.write(int(self.reset).to_bytes(1,'big'))
+    
+    def sendSyncPulse(self):
+        if self.portType == 'parallel':
+            self.port.setData(self.syncPulse)
+        elif self.portType == 'serial':
+            self.port.write(int(self.syncPulse).to_bytes(1,'big'))
+        if self.audioOn:
+            soundCh = self.syncSound.play()
+            while soundCh.get_busy():
+                pass
+        if self.portType == 'parallel':
+            core.wait(0.1)
+            self.port.setData(self.reset)
+        elif self.portType == 'serial':
+            core.wait(0.1)
+            self.port.write(int(self.reset).to_bytes(1,'big'))
+    
+    def sendSignal(self,signalCode):
+        if self.portType == 'parallel':
+            self.port.setData(signalCode)
+        elif self.portType == 'serial':
+            self.port.write(int(signalCode).to_bytes(1,'big'))
+
+
+
+def present_stimulus(stimInfo,exptInfo,displayText,receiver,toucher,saveFiles,exptClock,isiCountdown,goStopSound,sync):
+    silentLead = 0.064
+    countDownDuration = 3.0
+    stopDuration = 0.434
+    ppReset = 0
+    
+    # display messages
+    receiver.updateMessage(displayText['waitMessage'])
+    toucher.updateMessage(stimInfo['toucherCueText'])
+    
+    # get the audio cue file for this trial
+    thisCueSound = pygame.mixer.Sound(stimInfo['cueSound'])
+    thisSoundDuration = stimInfo['cueSoundDuration']
+    
+    # check if triggers needed
+    triggerOnNeeded = triggerOffNeeded = sync.portType != 'none'
+    
+    startLogNeeded = stopLogNeeded = True
+    
+    # wait for inter-stimulus interval duration
+    while isiCountdown.getTime() > thisSoundDuration + silentLead + countDownDuration:
+        toucher.updateTimerDisplay(isiCountdown.getTime())
+        for (key,keyTime) in event.getKeys(['escape'], timeStamped=exptClock):
+            saveFiles.logAbort(keyTime)
+            core.quit()
+    
+    # audio cue for toucher
+    soundCh = thisCueSound.play()
+    saveFiles.logEvent(exptClock.getTime(),'toucher cue {}' .format(stimInfo['stim']))
+    ## display messages
+    toucher.updateMessage(stimInfo['toucherCueText'] + '.\n'+ displayText['touchMessage'])
+    receiver.updateMessage(displayText['fixationMessage'])
+    while soundCh.get_busy():
+        toucher.updateTimerDisplay(isiCountdown.getTime())
+        for (key,keyTime) in event.getKeys(['escape'], timeStamped=exptClock):
+            soundCh.stop()
+            saveFiles.logAbort(keyTime)
+            core.quit()
+    
+    # signal the stimulus
+    soundCh = goStopSound.play()
+    saveFiles.logEvent(exptClock.getTime() + silentLead,'countdown to touch')
+    while soundCh.get_busy():
+        # check if the experiment is aborted
+        for (key,keyTime) in event.getKeys(['escape'], timeStamped=exptClock):
+            soundCh.stop()
+            saveFiles.logAbort(keyTime)
+            core.quit()
+        # start of the stimulus, audio 'go' signal
+        if isiCountdown.getTime() < 0:
+            toucher.hideTimerDisplay()
+            if triggerOnNeeded:
+                sync.sendSignal(stimInfo['SignalNo'])
+                stimStartTime = exptClock.getTime()
+                saveFiles.logEvent(stimStartTime, 'port signal sent: {}' .format(stimInfo['SignalNo']))
+                triggerOnNeeded = False
+            if startLogNeeded:
+                saveFiles.logEvent(stimStartTime,'start touching')
+                startLogNeeded = False
+            # end of the stimulus, audio 'stop' signal
+            if isiCountdown.getTime() < -10:
+                if triggerOffNeeded:
+                    sync.sendSignal(sync.endStim)
+                    stimStopTime = exptClock.getTime()
+                    saveFiles.logEvent(stimStopTime, 'port signal sent: {}' .format(sync.endStim))
+                    sync.sendSignal(sync.reset)
+                    triggerOffNeeded = False
+                if stopLogNeeded:
+                    isiCountdown.reset(exptInfo['03. Inter-stimulus interval (sec)'])
+                    saveFiles.logEvent(stimStopTime,'stop touching')
+                    stopLogNeeded = False
+        # keep updating the timer display before the stimulus starts, during audio countdown
+        elif stopLogNeeded: 
+            toucher.updateTimerDisplay(isiCountdown.getTime())
+    
+
+def get_response(stimLabels,receiverCueText,stimInfo,displayText,receiver,toucher,saveFiles,exptClock):
+    # wait for participant
+    toucher.updateMessage(displayText['waitMessage'])
+    
+    # present cue buttons for receiver to make a choice
+    receiver.updateMessage('')
+    
+    ## randomise button positions
+    randomStimLabels = random.sample(stimLabels,len(stimLabels))
+    receiver.showButtons([receiverCueText[i] for i in randomStimLabels])
+    saveFiles.logEvent(exptClock.getTime(),'buttons presented')
+    
+     # get response from receiver
+    (responseN,rTime) = receiver.getButtonClick(exptClock)
+    if responseN == -2:
+        saveFiles.logAbort(rTime)
+        core.quit()
+    elif responseN == -1:
+        response = 'timeout'
+    else:
+        response = randomStimLabels[responseN]
+    correctText = ['incorrect','correct']
+    saveFiles.logEvent(rTime,'receiver responded {} - {}' .format(response, correctText[int(stimInfo['stim']==response)]))
+    
+    # stop drawing buttons for receiver
+    receiver.hideButtons()
+    return(response)
+
+if __name__ == "__main__":
+    # demo of the button screens and getting input from mouse and then keyboard
+    items = ['attention','gratitude','love','sadness','happiness','calming']
+    receiverCueText = dict((line.strip().split('\t') for line in open('receiver-cues.txt')))
+    myInt = ButtonInterface(True,
+                            1,
+                            [1280,720],
+                            'wait',
+                            2,3,
+                            [receiverCueText[i] for i in items])
+    exptClock = core.Clock()
+    exptClock.reset()
+    #buttonOpacity = 1
+    
+    randomItems = random.sample(items, len(items))
+    myInt.showButtons([receiverCueText[i] for i in randomItems])
+    clock = core.Clock()
+    clock.reset()
+    (responseN,rTime) = myInt.getButtonClick(exptClock)
+    print(rTime)
+    if responseN == -2:
+        response = 'aborted'
+    elif responseN == -1:
+        response = 'timeout'
+    else:
+        response = randomItems[responseN]
+    print(response)
+    
+    myInt.hideButtons()
+    core.wait(2)
+    
+    randomItems = random.sample(items, len(items))
+    myInt.showButtons([receiverCueText[i] for i in randomItems])
+    
+    (response,rTime) = myInt.getSelection(20,clock)
+    
+    print(rTime)
+    if response == -2:
+        print('aborted')
+    elif response == -1:
+        print('timeout')
+    else:
+        print(randomItems[response])
+        
+    myInt.hideButtons()
+    core.wait(2)
+    
+    core.quit()
